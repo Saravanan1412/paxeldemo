2b0e610f9744f222821514c5b7afec28c85a7b0f
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-08 19:45:36 +0530

    Update profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,7 +7,7 @@ export const Profile = () => {
   return (
     <div className='flex flex-col min-h-[100vh]'>
       <Header/>
-        <div  className='text-white flex flex-col items-center  mt-44 text-center'>
+        <div  className='text-white flex flex-col items-center  mt-24 text-center'>
           <h1 className='text-4xl'>Profile</h1>
           <img src={Profilelogo} alt="profile" className='w-[100px] mt-16'/>
           <div className='flex flex-col justify-center mt-10 w-[600px] h-[350px] text-3xl leading-loose bg-[#5e50a1] rounded-3xl'>
