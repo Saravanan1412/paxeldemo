d234a11826ffe7bb889526362ce8a388f3c492fb
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-08 19:50:30 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import Paxelhome from '../assets/paxel.png'
 export const Header = () => {
     const navigate = useNavigate();
     const logout = () =>{navigate('/')}
-    const home=()=>{navigate('/DashBoard')}
+    const home=()=>{navigate('/DashBoard');window.scrollTo(0, 0)}
   return (
     <div className=' bg-white flex justify-between text-2xl p-5 fixed w-[100%] top-0'>
         <button onClick={home}><img src={Paxelhome} alt="homelogo"/></button>
