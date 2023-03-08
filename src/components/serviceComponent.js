7cef05acc41e20fa94ce9b0ac7c9229cf07f2bc7
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-08 20:04:48 +0530

    Update serviceComponent.js

--- src/components/serviceComponent.js
+++ src/components/serviceComponent.js
@@ -6,7 +6,7 @@ const ServiceComponent = (props) => {
     <div >
         <div className='bg-white w-[250px] h-[250px] text-black text-3xl border-b-8 border-[#ffb003] m-2'>
           <div className='pt-16'>
-            <img src={props.icon} alt="icon" className=' ml-10 w-[100px] h-[100px] '/>
+            <img src={props.icon} alt="icon" className=' ml-10 w-[50px] h-[50px] '/>
           </div>
           <p className='ml-10 mt-8'>{props.title}</p>
           <button className='ml-10 mt-4 text-xl text-white bg-[#00203f] w-20 rounded-xl' onClick={()=>{navigate(props.route);window.scrollTo(0, 0)}}>MORE</button>
